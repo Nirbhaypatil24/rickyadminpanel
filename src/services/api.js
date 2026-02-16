2b7931d4cf6c782d33453f8e89e387fd80e60b42
Nirbhay Khedekar <160317380+Nirbhaypatil24@users.noreply.github.com>
2026-02-16 22:03:28 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Single source of truth for backend URL (both dev & prod)
 const API_BASE_URL =
-  'https://ec2-3-110-29-119.ap-south-1.compute.amazonaws.com';
+  'https://3.110.29.119';
 
 console.log('📡 API Base URL:', API_BASE_URL);
 
