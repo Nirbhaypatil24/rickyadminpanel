5c980dac3c5bb3e5774eba210110c8561e234d73
Nirbhay Khedekar <160317380+Nirbhaypatil24@users.noreply.github.com>
2026-02-16 21:19:07 +0530

    Update BACKEND_URL to include '/api' path

--- api/[...path].js
+++ api/[...path].js
@@ -1,7 +1,7 @@
 // api/[...path].js
 import axios from 'axios';
 
-const BACKEND_URL = 'https://ec2-3-110-29-119.ap-south-1.compute.amazonaws.com';
+const BACKEND_URL = 'https://ec2-3-110-29-119.ap-south-1.compute.amazonaws.com/api';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
